9d0013fccbc60c319ffe17b03f6b265dafd06452
bugariu <bugariu@users.sourceforge.net>
2007-11-21 18:03:12 +0000

    - updated revision to 0.1.2-dev

--- WindowsMobile/Install/make.bat
+++ WindowsMobile/Install/make.bat
@@ -1,4 +1,4 @@
 cabwiz NaviPOWM.inf
-ezsetup.exe -l english -i NaviPOWM.ini -r InstReadMe.txt -e ..\..\LICENSE.TXT -o NaviPOWM-Mobile-0.1.1-Setup.exe
-move NaviPOWM-Mobile-0.1.1-Setup.exe Install
-move NaviPOWM.cab Install\NaviPOWM-Mobile-0.1.1-Setup.cab
+ezsetup.exe -l english -i NaviPOWM.ini -r InstReadMe.txt -e ..\..\LICENSE.TXT -o NaviPOWM-Mobile-0.1.2-dev-Setup.exe
+move NaviPOWM-Mobile-0.1.2-dev-Setup.exe Install
+move NaviPOWM.cab Install\NaviPOWM-Mobile-0.1.2-dev-Setup.cab
--- common/src/CXNaviPOWM.cpp
+++ common/src/CXNaviPOWM.cpp
@@ -32,7 +32,7 @@
 #include "CXDeviceContext.hpp"
 #include "Utils.hpp"
 
-const char * VERSIONSTRING ="NaviPOWM 0.1.1";
+const char * VERSIONSTRING ="NaviPOWM 0.1.2-dev";
 const char * INFOSTRING1 ="(C) Doru-Julian Bugariu";
 const char * INFOSTRING2 ="http://sourceforge.net/projects/navipowm";
 
