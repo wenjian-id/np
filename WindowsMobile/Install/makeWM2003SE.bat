fafd706f99f9e2d49eaf4e3d219ab87265d724ef
bugariu <bugariu@users.sourceforge.net>
2010-03-05 20:19:05 +0000

    - updated version number

--- WindowsMobile/Install/makeWM2003SE.bat
+++ WindowsMobile/Install/makeWM2003SE.bat
@@ -1,4 +1,4 @@
 cabwiz NaviPOWM.inf
-ezsetup.exe -l english -i NaviPOWM.ini -r InstReadMe.txt -e ..\..\LICENSE.TXT -o NaviPOWM-Mobile-WM2003SE-0.2.4-Setup.exe
-move NaviPOWM-Mobile-WM2003SE-0.2.4-Setup.exe Install
-move NaviPOWM.cab Install\NaviPOWM-Mobile-WM2003SE-0.2.4-Setup.cab
+ezsetup.exe -l english -i NaviPOWM.ini -r InstReadMe.txt -e ..\..\LICENSE.TXT -o NaviPOWM-Mobile-WM2003SE-0.2.5-Setup.exe
+move NaviPOWM-Mobile-WM2003SE-0.2.5-Setup.exe Install
+move NaviPOWM.cab Install\NaviPOWM-Mobile-WM2003SE-0.2.5-Setup.cab
