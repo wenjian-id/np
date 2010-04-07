c26e2565a3bfafa3d66a1407d625ed0ba27a1ba1
bugariu <bugariu@users.sourceforge.net>
2010-04-07 12:42:48 +0000

    - updated version number

--- common/src/CXNaviPOWM.cpp
+++ common/src/CXNaviPOWM.cpp
@@ -39,7 +39,7 @@
 #include "CXPOWMMap.hpp"
 #include "CXMapMovingDetails.hpp"
 
-const char * VERSIONSTRING ="NaviPOWM 0.2.5-dev1";
+const char * VERSIONSTRING ="NaviPOWM 0.2.5-dev2";
 const char * INFOSTRING1 ="(C) Doru Julian Bugariu";
 const char * INFOSTRING2 ="http://sourceforge.net/projects/navipowm";
 
